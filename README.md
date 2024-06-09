5cdff839bba839b805290b62c063e070f204fcf3
Rusty Nail <rustynail@Elviras-MacBook-Pro.local>
2024-06-09 18:54:45 +0400

    identify customers against zone annotations

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ process a video file and output results.
 
 ## Features:
 
-- \[ \] Identify number of unique customers in the shop
-- \[ \] Track each customer
+- \[x\] Identify number of unique customers in the shop
+- \[x\] Track each customer
 - \[ \] Identify how many people entered and exit shop during the video
 - \[ \] Identify gender (male/female) and age (child/adult as classification) for each customer
 - \[ \] Identify number of unique customers that made a purchase
@@ -16,6 +16,87 @@ process a video file and output results.
 
 ### Installation
 
-1. Install poetry and use system env
-1. Install torch and torchvision with conda
-1. Install deps
+1. Install [ poetry ](https://python-poetry.org/docs/basic-usage/) and use system env (in case using conda or other):
+
+```
+poetry env use system
+```
+
+2. Install torch and torchvision with conda (or other):
+
+```
+conda install pytorch torchvision -c pytorch
+```
+
+3. Install dependencies (poetry is recommended but requirements.txt is also available):
+
+```
+poetry install
+```
+
+4. Install ffmpeg as per system.
+
+### Tools and packages
+
+[Fiftyone](https://docs.voxel51.com/index.html) is used as the main tool for managing data
+and labels. [Fire](https://google.github.io/python-fire/guide/) is the main cli entrypoint
+to run all scripts. User inputs are validated with
+[pydantic](https://docs.pydantic.dev/latest/concepts/models/).
+
+The `FiftyoneDataset` is the main entripoint to run all commands.
+Check out its usage below:
+
+```
+NAME
+    main.py FiftyoneDataset - Usage docs: https://docs.pydantic.dev/2.7/concepts/models/
+
+SYNOPSIS
+    main.py FiftyoneDataset GROUP | COMMAND | <flags>
+
+FLAGS
+    -n, --name=NAME (required)
+        Type: str
+    -o, --overwrite=OVERWRITE
+        Type: bool
+        Default: False
+
+```
+
+### Run the commands
+
+1. Create a fiftyone dataset to visualize data and labels:
+
+```
+pymn FiftyoneDataset --name <name> create <path/to/video/folder>
+```
+
+2. Run detection and tracking with YOLOv8 and Boxmot:
+
+```
+pymn FiftyoneDataset --name <name> track --model yolov8s --label-field yolov8s
+```
+
+3. Count and track customers
+
+To be able to identify a customer we need to annotate
+a cashier's zone in a yaml config file. Once a fiftyone
+dataset is created, find out video sample id from web UI
+and add an entry to the [ config file ](configs/annotations.yaml)
+following the existing example.
+
+3.1. Add these annotations to fiftyone:
+
+```
+pymn FiftyoneDataset --name <name> annotate_zones configs/annotations.yaml --label_field zone
+```
+
+3.2. After adding zones, run the following command to label
+each person as a cashier or a customer:
+
+```
+❯ pymn FiftyoneDataset --name <name> identify_customer --tracking_field yolov8s --zone_field zone --iou-threshold 0.5
+
+Output:
+6665610ec7102da65e0f95ef cashier counts: 2
+6665610ec7102da65e0f95ef customer counts: 41
+```
--- src/data_viz.py
+++ src/data_viz.py
@@ -1,20 +1,24 @@
 """Use fiftyone as the main data visualization tool."""
 
+from collections import defaultdict
 from pathlib import Path
 
 import fiftyone as fo
 import fiftyone.types as fot
 import fiftyone.utils.ultralytics as fouu
 import pydantic
+import yaml
 from termcolor import cprint
 from ultralytics import YOLO
 
 
+def _load_yaml(path: str):
+    with open(path) as fp:
+        return yaml.safe_load(fp)
+
+
 class UltralyticsModel(pydantic.BaseModel):
     name: str = pydantic.Field("yolov8s.pt", description="Ultralytics model name")
-    # classes: list[str] = pydantic.Field(
-    #     ["person"], description="List of classes to detect"
-    # )
     conf_threshold: float = 0.25
     iou_threshold: float = 0.45
     image_size: int = 640
@@ -35,6 +39,30 @@ class UltralyticsModel(pydantic.BaseModel):
             stream=True,
             show=False,
         )
+
+
+class AnnotatedZone(pydantic.BaseModel):
+    smp_id: str
+    cashier: list[float]
+    exit: list[float]
+
+    @pydantic.field_validator("cashier", "exit")
+    def check_values(val: list[float]):
+        if not len(val) == 4:
+            raise ValueError("wrong number of inputs in the bbox")
+        if not all([0 <= i <= 1 for i in val]):
+            raise ValueError("values must be between 0 and 1")
+        return val
+
+    def to_detections(self):
+        """convert zone to fiftyone detection format"""
+        dets = []
+        for zone in ["cashier", "exit"]:
+            x0, y0, x1, y1 = getattr(self, zone)
+            dets.append(
+                fo.Detection(label=zone, bounding_box=[x0, y0, x1 - x0, y1 - y0])
+            )
+        return fo.Detections(detections=dets)
 
 
 class FiftyoneDataset(pydantic.BaseModel):
@@ -82,7 +110,86 @@ class FiftyoneDataset(pydantic.BaseModel):
                 smp.frames[frame_no][label_field] = detections
                 frame_no += 1
             smp.save()
+        cprint(f"Finished tracking for {len(self.dataset)} samples", "green")
 
+    def annotate_zones(self, annotations: str, label_field: str = "zone"):
+        """Use config file with zone annotations to add to fiftyone"""
+        self._load_dataset()
+        if self.dataset.has_frame_field(label_field):
+            self.dataset.delete_frame_field(label_field)
 
-if __name__ == "__main__":
-    FiftyoneDataset(name="AIQ").track(label_field="yolob8s")
+        zone_config = _load_yaml(annotations).get("zones", [])
+        for conf in zone_config:
+            zone = AnnotatedZone(**conf)
+            smp = self.dataset[zone.smp_id]
+            for frame_id in smp.frames:
+                smp.frames[frame_id][label_field] = zone.to_detections()
+            smp.save()
+        cprint(f"Annotated zones for {len(zone_config)} samples", "green")
+
+    def identify_customer(
+        self,
+        tracking_field: str,
+        zone_field: str,
+        new_field: str = "visitor_type",
+        iou_threshold: float = 0.5,
+    ):
+        """Classify each track id as a customer or cashier base on zone intersection"""
+        self._load_dataset()
+        n_matches = {}
+        view = self.dataset.filter_labels(
+            f"frames.{ tracking_field }", fo.ViewField("label") == "person"
+        )
+        # count iou matching with cashier
+        for smp in view.select_fields(
+            [f"frames.{ tracking_field }", f"frames.{ zone_field }"]
+        ):
+            n_matches[smp.id] = defaultdict(lambda: 0)
+            for frame_id in smp.frames:
+                detections = smp[frame_id][tracking_field].detections
+                zone = list(
+                    filter(
+                        lambda x: x.label == "cashier",
+                        smp[frame_id][zone_field].detections,
+                    )
+                )[0]
+                zone_x, zone_y, zone_w, zone_h = zone.bounding_box
+                for det in detections:
+                    x, y, w, h = det.bounding_box
+                    bbox_center = (x + w / 2, y + h / 2)
+                    within_x = zone_x < bbox_center[0] < zone_x + zone_w
+                    within_y = zone_y < bbox_center[1] < zone_y + zone_h
+                    n_matches[smp.id][det.index] += int(within_x and within_y)
+
+        # update class labels for customer and cashiers
+        for smp in view.select_fields(f"frames.{tracking_field}"):
+            smp_view = self.dataset.select(smp.id)
+            totals = smp_view.count_values(f"frames.{tracking_field}.detections.index")
+            for frame_id in smp.frames:
+                detections = []
+                for det in smp.frames[frame_id][tracking_field].detections:
+                    if det.index not in n_matches[smp.id]:
+                        continue
+
+                    is_cashier = (
+                        n_matches[smp.id][det.index] / totals[det.index] > iou_threshold
+                    )
+                    label = "cashier" if is_cashier else "customer"
+                    detections.append(
+                        fo.Detection(
+                            label=label,
+                            bounding_box=det.bounding_box,
+                            confidence=det.confidence,
+                            index=det.index,
+                        )
+                    )
+                smp[frame_id][new_field] = fo.Detections(detections=detections)
+
+            smp.save()
+
+            # print counts of customers
+            for label in ["cashier", "customer"]:
+                n_distinct = smp_view.filter_labels(
+                    f"frames.{new_field}", fo.ViewField("label") == label
+                ).distinct(f"frames.{new_field}.detections.index")
+                cprint(f"{smp.id} {label} counts: {len(n_distinct)}", "green")
